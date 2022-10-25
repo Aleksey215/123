90d95d3fcc7645059d5e085a9e2cd1ef44e082b9
Aleksey215 <kaloha215@gmail.com>
2022-10-25 19:59:47 +1000

    Added a page with all the ads created by the user to the profile. Added a page with detailed information about the ad and all the responses to the selected ad. I also made it possible to confirm and delete responses on the same page. To do this, I created several views and a response confirmation function

--- Game_Site/profile/urls.py
+++ Game_Site/profile/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, UsersPostsView, ResponsesForPostView, confirm_response
 
 # перенаправляемся на единственное представление IndexView, которое описано в соответствующем файле views.py
 urlpatterns = [
-    path('', IndexView.as_view()),
-]
+    path('', IndexView.as_view(), name='profile'),
+    path('user_posts/', UsersPostsView.as_view(), name='users_posts'),
+    path('responses_for_post/<int:pk>', ResponsesForPostView.as_view(), name='response_for_post'),
+    path('responses_for_post/<int:pk>/confirm_response/', confirm_response, name='confirm_response'),
+    ]
--- Game_Site/profile/views.py
+++ Game_Site/profile/views.py
@@ -1,8 +1,41 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import redirect
+from django.views.generic import TemplateView, ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+from ads.models import Post, Response
 
 
 # ограничение доступа к странице с профилем (только для зарегистрированных)
 class IndexView(LoginRequiredMixin, TemplateView):
     template_name = 'profile/profile.html'
+
+
+class UsersPostsView(ListView):
+    model = Post
+    template_name = 'profile/user_posts.html'
+    context_object_name = 'posts'
+    ordering = ['-id']
+
+
+class ResponsesForPostView(DetailView):
+    model = Post
+    template_name = 'profile/responses_for_post.html'
+
+    def get_context_data(self, **kwargs):
+        # в своих объявлениях пользователь может редактировать их
+        # в чужих может оставить отклик
+        context = super().get_context_data(**kwargs)
+        id = self.kwargs.get('pk')
+        obj = Post.objects.get(pk=id)
+        responses = Response.objects.filter(post=obj)
+        context['responses'] = responses
+        return context
+
+
+def confirm_response(request, **kwargs):
+    pk = request.GET.get('pk', )
+    response = Response.objects.get(pk=pk)
+    post_id = response.post.pk
+    response.confirmed = True
+    response.save()
+    return redirect(f'/profile/responses_for_post/{post_id}')
